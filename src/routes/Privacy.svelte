3cc3c53b1e2ca9a7502f0b79f3bb457f941c3e25
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2022-12-23 22:00:27 +0800

    privacy thing scrollable

--- src/routes/Privacy.svelte
+++ src/routes/Privacy.svelte
@@ -1,6 +1,19 @@
 <script>
-	import { tick } from "svelte";
+	import { onMount, tick } from "svelte";
+  import { centerScroll } from "../lib/helper";
+  import { sn } from "../lib/shared";
 	import { showAds } from "./stores";
+
+	onMount(()=>{
+		const id = Math.random().toString(32);
+
+		sn.add(id, { selector: "#Privacy_page [tabindex]" });
+		sn.focus(id);
+
+		return () => {
+			sn.remove(id);
+		};
+	})
 </script>
 
 <svelte:window
@@ -12,23 +25,23 @@
 	}}
 />
 
-<main>
+<main id="Privacy_page" on:focus|capture={({ target }) => centerScroll(target)}>
 	<div class="title">Privacy Policy</div>
-	<div class="content">
+	<div tabindex="0" class="content">
 		Discord4KaiOS does not collect data whatsoever... This app uses Discord's API, and it is probably collecting data
 		and that's not my problem.
 	</div>
-	<div class="content">
+	<div tabindex="0" class="content">
 		However, a few privacy things are not implemented in this app because of the limitations of KaiOS.
 	</div>
-	<div class="content">
+	<div tabindex="0" class="content">
 		Discord creates a proxy for gifs called "GifV". It converts gifs to videos. However, on KaiOS devices gifv is very
 		slow. To make messages that contain gifs work, Discord4KaiOS will directly embed the gif images from the gif
-		provider (Ex. tenor). When debugging the app on desktop, a third party CORS proxy will be used to substitute KaiOS'
-		systemXHR permission.
+		<!-- svelte-ignore missing-declaration -->
+		provider (Ex. tenor). {#if !PRODUCTION}When debugging the app on desktop, a third party CORS proxy will be used to substitute KaiOS' systemXHR permission.{/if}
 	</div>
 	{#if $showAds}
-		<div class="content">
+		<div tabindex="0" class="content">
 			KaiAds are required for the app to be uploaded to the KaiStore. It is only present in the About page. The banner
 			ad is sandboxed in a cross-origin iframe, therefore it cannot access the app's data. The sandbox also means it
 			only connects to KaiOStech servers when you go to the page. To get rid of KaiAds, consider installing the app
